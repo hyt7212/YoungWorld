f174b8bbe16764307f67a67f3b00c95f42acdbbf
HuangYoung <sean0722@msn.com>
2016-05-30 21:48:12 +0800

    移动相机暂存

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float rotationSpeed = 10.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		transform.Rotate(Vector3.up *Time.deltaTime * TranslateSpeed);//绕Y轴 旋转 
+
+		float translation = Input.GetAxis("Vertical") * moveSpeed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+	}
+}
