9a1eed8b6b11cc43544b72be6107da2bf5bc702d
HuangYoung <sean0722@msn.com>
2016-05-31 21:27:26 +0800

    恢复5.3引擎版本，实现相机跟随移动

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,11 @@ public class Player : MonoBehaviour {
 
 	public float moveSpeed = 10f;
 	public float rotationSpeed = 10.0F;
-	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-//		transform.Rotate(Vector3.up *Time.deltaTime * TranslateSpeed);//绕Y轴 旋转 
-
 		float translation = Input.GetAxis("Vertical") * moveSpeed;
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 		translation *= Time.deltaTime;
