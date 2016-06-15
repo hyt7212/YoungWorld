c34dc238702e40d6dd9a0c665d40edaa704f6492
HuangYoung <sean0722@msn.com>
2016-06-15 09:22:17 +0800

    发射子弹

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public GameObject shellPfb;
+	public Transform shellPoint;
+	public float shootPower;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Shoot ();
+		}
+	}
+
+	void Shoot () {
+		GameObject newShell = Instantiate (shellPfb, shellPoint.position, shellPoint.rotation) as GameObject;
+		Rigidbody r = newShell.GetComponent<Rigidbody> ();
+		r.velocity = shellPoint.forward * shootPower;
+	}
+}
